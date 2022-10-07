81340b985fe25b6811b00eab6166d96d08835878
Alex-C <alexcook1613@gmail.com>
2022-10-07 18:39:01 -0400

    updated columns and rows for music-player and homepage. Added Goals page.

---
+++ Assets/js/check-box.js
@@ -0,0 +1,30 @@
+$("#checkbox-container :checkbox").on("change", function(){
+    alert("The checkbox with the ID '" + this.id + "' changed");
+});
+
+localStorage.setItem("favoriteVillan", "Dr. Hannibal Lecter");
+
+console.log(localStorage.getItem("favoriteVillan"));
+
+localStorage.removeItem("favoriteVillan");
+
+console.log(localStorage.getItem("favoriteVillan"));
+
+var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {};
+var $checkboxes = $("#checkbox-container :checkbox");
+
+$checkboxes.on("change", function(){
+  $checkboxes.each(function(){
+    checkboxValues[this.id] = this.checked;
+  });
+  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+});
+
+var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues');
+if (checkboxValues === null){
+  checkboxValues = {};
+}
+
+$.each(checkboxValues, function(key, value) {
+  $("#" + key).prop('checked', value);
+});
