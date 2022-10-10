a5c0d0e8070ab7b639610f7e1cf800a91a12ccc2
Alex-C <alexcook1613@gmail.com>
2022-10-10 15:11:43 -0400

    updated homepage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,7 @@ var aAudio = document.getElementById('playSong');
 
 function myAudioFunction(letter) {
     console.log("hello");
+    console.log("everything is going to be ok, my friend :)");
     if(letter == 'A') {
         aAudio.play();
     } else if(letter == 'b') {
